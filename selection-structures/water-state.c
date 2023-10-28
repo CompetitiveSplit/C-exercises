4a206f906fd3e88afe755512d9441a454adf1a50
CompetitiveSplit <narvaez0526@gmail.com>
2023-10-27 21:34:44 -0500

    selection-structures-problems

---
+++ selection-structures/water-state.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int water_temperature;
+
+  printf("Gime the temperature of the water: ");
+  scanf("%d", &water_temperature);
+
+  if (water_temperature <= 0) {
+    printf("The state of the water is solid\n");
+  } else if (water_temperature > 1 && water_temperature <= 100) {
+    printf("The state of the water is liquid\n");
+  }else{
+    printf("The state of the water is gas \n");
+  }
+}
